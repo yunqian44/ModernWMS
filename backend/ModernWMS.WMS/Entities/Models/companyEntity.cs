f269ae55e4a5e17f574551ac54f39a5e20059773
amo <amo@amo.com>
2022-12-19 10:34:24 +0800

    测试提交

---
+++ backend/ModernWMS.WMS/Entities/Models/companyEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YK.Entities.TiXi.Models
+{
+    /// <summary>
+    /// 刻章申请 实体
+    /// </summary>
+    [Table("company")]
+    public class companyEntity
+    {
+    }
+}
