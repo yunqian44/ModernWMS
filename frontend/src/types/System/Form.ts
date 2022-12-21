606bc2d49bfecaaefe05c7ab84356e2fcb8a31a8
liufu <809388027@qq.com>
2022-12-21 17:43:40 +0800

    feat(File): Unified Folder Name

---
+++ frontend/src/types/System/Form.ts
@@ -0,0 +1,21 @@
+export interface UniformFileNaming {
+  creator?: string
+  create_time?: string
+  last_update_time?: string
+  tenant_id?: number
+}
+
+// VxeTable Required paging parameters
+export interface TablePage {
+  total: number
+  pageIndex: number
+  pageSize: number
+}
+
+// API Required paging parameters
+export interface PageConfigProps {
+  pageIndex: number
+  pageSize: number
+  sqlTitle?: string
+  searchObjects?: any
+}
