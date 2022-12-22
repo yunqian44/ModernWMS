7f27f6c0f642b066f308a81cbf7ed692bcf7b174
Jokerls波吉 <11269866+jokerls-poji@user.noreply.gitee.com>
2022-12-22 09:47:37 +0800

    feat(Base):Add form dialog in freight setting.

---
+++ frontend/src/api/base/freightSetting.ts
@@ -0,0 +1,33 @@
+import http from '@/utils/http/request'
+import { PageConfigProps } from '@/types/System/Form'
+import { FreightVO } from '@/types/Base/freight'
+
+// Find Data by Pagination
+export const getFreightList = (data: PageConfigProps) => http({
+    url: '/freight/list',
+    method: 'post',
+    data
+  })
+
+// Add a new freight
+export const addFreight = (data: FreightVO) => http({
+    url: '/freight',
+    method: 'post',
+    data
+  })
+
+// Update freight
+export const updateFreight = (data: FreightVO) => http({
+    url: '/freight',
+    method: 'put',
+    data
+  })
+
+// Delete freight
+export const deleteFreight = (id: number) => http({
+    url: '/freight',
+    method: 'delete',
+    params: {
+      id
+    }
+  })
