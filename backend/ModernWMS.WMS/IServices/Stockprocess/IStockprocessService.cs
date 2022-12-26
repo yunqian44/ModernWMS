3357e0fca7ca7fd1d69c530ad63bed69cd2cba91
JawerZeng <790899040@qq.com>
2022-12-26 11:29:03 +0800

    stock process base api

---
+++ backend/ModernWMS.WMS/IServices/Stockprocess/IStockprocessService.cs
@@ -0,0 +1,60 @@
+/*
+ * date：2022-12-23
+ * developer：NoNo
+ */
+ using ModernWMS.Core.Services;
+ using ModernWMS.Core.Models;
+ using ModernWMS.Core.JWT;
+ using ModernWMS.WMS.Entities.Models;
+ using ModernWMS.WMS.Entities.ViewModels;
+ 
+ namespace ModernWMS.WMS.IServices
+ {
+     /// <summary>
+     /// Interface of StockprocessService
+     /// </summary>
+     public interface IStockprocessService : IBaseService<StockprocessEntity>
+     {
+         #region Api
+         /// <summary>
+         /// page search
+         /// </summary>
+         /// <param name="pageSearch">args</param>
+         /// <param name="currentUser">current user</param>
+         /// <returns></returns>
+         Task<(List<StockprocessViewModel> data, int totals)> PageAsync(PageSearch pageSearch, CurrentUser currentUser);
+         /// <summary>
+         /// Get all records
+         /// </summary>
+         /// <returns></returns>
+         Task<List<StockprocessViewModel>> GetAllAsync(CurrentUser currentUser);
+         /// <summary>
+         /// Get a record by id
+         /// </summary>
+         /// <param name="id">primary key</param>
+         /// <returns></returns>
+         Task<StockprocessWithDetailViewModel> GetAsync(int id);
+         /// <summary>
+         /// add a new record
+         /// </summary>
+         /// <param name="viewModel">viewmodel</param>
+         /// <param name="currentUser">current user</param>
+         /// <returns></returns>
+         Task<(int id, string msg)> AddAsync(StockprocessViewModel viewModel, CurrentUser currentUser);
+         /// <summary>
+         /// update a record
+         /// </summary>
+         /// <param name="viewModel">viewmodel</param>
+         /// <returns></returns>
+         Task<(bool flag, string msg)> UpdateAsync(StockprocessViewModel viewModel);
+ 
+         /// <summary>
+         /// delete a record
+         /// </summary>
+         /// <param name="id">id</param>
+         /// <returns></returns>
+         Task<(bool flag, string msg)> DeleteAsync(int id);
+         #endregion
+     }
+ }
+ 
