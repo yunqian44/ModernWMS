d2b697c1290fc2accbdbf0e36032d1ab747fbc29
amo <amo@amo.com>
2022-12-31 13:02:12 +0800

    stockadjust list api

--- backend/ModernWMS.WMS/Entities/ViewModels/Stockadjust/StockadjustViewModel.cs
+++ backend/ModernWMS.WMS/Entities/ViewModels/Stockadjust/StockadjustViewModel.cs
@@ -32,6 +32,12 @@ namespace ModernWMS.WMS.Entities.ViewModels
         public int id { get; set; }  = 0;
 
         /// <summary>
+        /// job_type
+        /// </summary>
+        [Display(Name = "job_type")]
+        public byte job_type { get; set; } = 0;
+
+        /// <summary>
         /// job_code
         /// </summary>
         [Display(Name = "job_code")]
@@ -39,22 +45,57 @@ namespace ModernWMS.WMS.Entities.ViewModels
         public string job_code { get; set; }  = string.Empty;
 
         /// <summary>
+        /// spu_code
+        /// </summary>
+        public string spu_code { get; set; } = string.Empty;
+
+        /// <summary>
+        /// spu_name
+        /// </summary>
+        public string spu_name { get; set; } = string.Empty;
+
+        /// <summary>
         /// sku_id
         /// </summary>
         [Display(Name = "sku_id")]
-        public int sku_id { get; set; }  = 0;
+        public int sku_id { get; set; } = 0;
+
+        /// <summary>
+        /// sku_code
+        /// </summary>
+        public string sku_code { get; set; } = string.Empty;
+
+        /// <summary>
+        /// sku_name
+        /// </summary>
+        public string sku_name { get; set; } = string.Empty;
 
         /// <summary>
         /// goods_owner_id
         /// </summary>
         [Display(Name = "goods_owner_id")]
-        public int goods_owner_id { get; set; }  = 0;
+        public int goods_owner_id { get; set; } = 0;
+
+        /// <summary>
+        /// goods owner's name
+        /// </summary>
+        public string goods_owner_name { get; set; } = string.Empty;
 
         /// <summary>
         /// goods_location_id
         /// </summary>
         [Display(Name = "goods_location_id")]
-        public int goods_location_id { get; set; }  = 0;
+        public int goods_location_id { get; set; } = 0;
+
+        /// <summary>
+        /// warehouse_name
+        /// </summary>
+        public string warehouse_name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// location_name
+        /// </summary>
+        public string location_name { get; set; } = string.Empty;
 
         /// <summary>
         /// qty
@@ -93,13 +134,7 @@ namespace ModernWMS.WMS.Entities.ViewModels
         /// is_update_stock
         /// </summary>
         [Display(Name = "is_update_stock")]
-        public bool is_update_stock { get; set; } =true;
-
-        /// <summary>
-        /// job_type
-        /// </summary>
-        [Display(Name = "job_type")]
-        public byte job_type { get; set; }  = 0;
+        public bool is_update_stock { get; set; } =false;
 
         /// <summary>
         /// source_table_id
