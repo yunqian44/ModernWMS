83536e48fa71ec0373257cc3115a47d148ed4255
Jokerls波吉 <11269866+jokerls-poji@user.noreply.gitee.com>
2023-01-05 19:26:23 +0800

    feat(Home):Add homepage.

--- frontend/src/view/home/homepage/homepage.vue
+++ frontend/src/view/home/homepage/homepage.vue
@@ -1,10 +1,67 @@
 <template>
   <div class="container">
-    welcome Homepage!
+    <div class="textBox">
+      <div class="mainTitle">ModernWMS</div>
+      <div class="subTitle">现代化仓库管理系统</div>
+    </div>
+    <div class="content">
+      <div class="lottieBox">
+        <lottie :src="dataJson"></lottie>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import lottie from '@/components/lottie.vue'
+import dataJson from '@/assets/warehouse.json'
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  align-items: center;
+}
+
+.content {
+  width: 65%;
+}
+
+.textBox {
+  width: 100%;
+  position: absolute;
+  left: 50%;
+  top: 5%;
+  transform: translate(-50%, 0);
+  letter-spacing: 5px;
+  z-index: 999;
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+.mainTitle{
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  color: #9c27b0;
+  font-weight: bold;
+  font-size: 120px;
+}
+
+.subTitle{
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  font-size: 30px;
+  letter-spacing: 5px;
+  color: #c9a7cf;
+}
+
+.lottieBox {
+  min-width: 100%;
+  max-width: 100%;
+}
+</style>
