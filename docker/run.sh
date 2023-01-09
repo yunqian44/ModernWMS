bb579a4532e9cef6096e052055cee46e37de94d4
leucoon <616262459@qq.com>
2023-01-09 04:58:52 +0000

    update docker/run.sh.
    Signed-off-by: leucoon <616262459@qq.com>

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,7 @@
 # start 1
 /usr/sbin/nginx -g 'daemon off;' > /var/log/start1.log 2>&1 &
 # start 2
-dotnet /app/ModernWMS.dll --urls http://0.0.0.0:21011 > /var/log/start2.log 2>&1 &
+dotnet /app/ModernWMS.dll --urls http://0.0.0.0:20011 > /var/log/start2.log 2>&1 &
 # just keep this script running
 while [[ true ]]; do
     sleep 1
